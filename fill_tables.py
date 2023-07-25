a9543e87913eceadc44e8315e264055fe39a5290
AJKhan-e6 <akhan@e6x.io>
2023-07-25 18:36:36 +0530

    Made it so control goes back to shell after execution

--- fill_tables.py
+++ fill_tables.py
@@ -13,31 +13,35 @@ db_params = {
 db_name = "imdb"
 csv_folder = "./imdb-benchmark/"
 
-conn = psycopg2.connect(**db_params)
+def fill_tables():
+    conn = psycopg2.connect(**db_params)
 
 
 
-# Loop through all CSV files in the folder
-for file in os.listdir(csv_folder):
-    if file.endswith(".csv"):
-        table_name = os.path.splitext(file)[0]
+    # Loop through all CSV files in the folder
+    for file in os.listdir(csv_folder):
+        if file.endswith(".csv"):
+            table_name = os.path.splitext(file)[0]
 
-        # Create a cursor to execute SQL commands
-        cursor = conn.cursor()
+            # Create a cursor to execute SQL commands
+            cursor = conn.cursor()
 
-        # Construct the COPY command and execute it
-        try:
-            copy_command = f"COPY {table_name} FROM STDIN WITH (FORMAT csv, HEADER false, ESCAPE '\\', ENCODING 'utf-8', QUOTE '\"');"
-            with open(os.path.join(csv_folder, file), 'r') as csv_file:
-                cursor.copy_expert(copy_command, csv_file)
-            conn.commit()
-            print(f"Data imported into table {table_name}")
-        except Exception as e:
-            conn.rollback()
-            print(f"Error: Unable to import data into table {table_name} - {e}")
+            # Construct the COPY command and execute it
+            try:
+                copy_command = f"COPY {table_name} FROM STDIN WITH (FORMAT csv, HEADER false, ESCAPE '\\', ENCODING 'utf-8', QUOTE '\"');"
+                with open(os.path.join(csv_folder, file), 'r') as csv_file:
+                    cursor.copy_expert(copy_command, csv_file)
+                conn.commit()
+                print(f"Data imported into table {table_name}")
+            except Exception as e:
+                conn.rollback()
+                print(f"Error: Unable to import data into table {table_name} - {e}")
 
-        # Close the cursor
-        cursor.close()
+            # Close the cursor
+            cursor.close()
 
-# Close the connection to the database
-conn.close()
+    # Close the connection to the database
+    conn.close()
+
+if __name__ == "__main__":
+    fill_tables()
